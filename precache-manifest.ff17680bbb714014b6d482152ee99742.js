2344a5df02721b28a5bf180f7df2662cb7f24d9d
Marfusha-91 <rovena91@mail.ru>
2022-10-14 18:47:33 +0300

    Updates

---
+++ precache-manifest.ff17680bbb714014b6d482152ee99742.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d974cb4bec55c1b9bbd59fe790bb856f",
+    "url": "/todolist_lesson10/index.html"
+  },
+  {
+    "revision": "fab8f60245af1e5bb267",
+    "url": "/todolist_lesson10/static/css/main.aa207b39.chunk.css"
+  },
+  {
+    "revision": "3bdd75b5d82994c75c56",
+    "url": "/todolist_lesson10/static/js/2.847be3ce.chunk.js"
+  },
+  {
+    "revision": "936711c3f4be0c9c88207c43cda44117",
+    "url": "/todolist_lesson10/static/js/2.847be3ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fab8f60245af1e5bb267",
+    "url": "/todolist_lesson10/static/js/main.d93dda18.chunk.js"
+  },
+  {
+    "revision": "4268ef2829174b9bd259",
+    "url": "/todolist_lesson10/static/js/runtime-main.249d4bc7.js"
+  }
+]);
